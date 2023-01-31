e0f6f2b7650983ae4c007238005ac399988594e1
felix igwe <felixigwe55@gmail.com>
2023-01-31 05:12:00 +0100

    hash

---
+++ 0x1A-hash_tables/1-my_list.py
@@ -0,0 +1,8 @@
+ss MyList"""
+
+class MyList(list):
+    """Type clas#!/usr/bin/python3
+"""Define an inherited list clas MyList with print_sorted function"""
+
+    def print_sorted(self):
+        print(sorted(self))
